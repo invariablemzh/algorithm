b843e8775913a2e144e2c9884f6fce9746797aa9
invariablemzh <invariablemzh@gmail.com>
2022-10-02 19:00:18 +0800

    算法

--- 代码/st/198. 反素数(约数个数).cpp
+++ 代码/st/198. 反素数(约数个数).cpp
@@ -10,8 +10,29 @@ typedef pair<int,int> PII;
     cin.tie(0);                                                                                                        \
     cout.tie(0);
 
-void solve(){
+const int N = 1000010;
 
+int n;
+int maxd, number;
+int primes[] = {2,3,5,7,11,13,17,19,23};
+
+void dfs(int u,int last,int p,int s){
+    if(s > maxd || s == maxd && p < number){
+        maxd = s;
+        number = p;
+    }
+    if(u==9) return;
+    for(int i = 1;i<=last;i++){
+        if((ll)primes[u] * p > n) break;
+        p *= primes[u];
+        dfs(u+1,i,p,s*(i+1));
+    }
+}
+
+void solve(){
+    cin >> n;
+    dfs(0,30,1,1);
+    cout << number << '\n';
 }
 
 int main(){
